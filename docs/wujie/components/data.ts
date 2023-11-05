9250066881396617ba4adbde40a8feb982247e59
tangqi <616992443@qq.com>
2023-11-05 21:41:04 +0800

    feat: init

---
+++ docs/wujie/components/data.ts
@@ -0,0 +1,30 @@
+export const wujieList = [
+  {
+    name: "无极",
+    url: "https://wujicode.cn/xy/app/prod/official/index",
+  },
+  {
+    name: "UmiJS",
+    url: "https://umijs.org/",
+  },
+  {
+    name: "React",
+    url: "https://react.dev/",
+  },
+  {
+    name: "Webpack",
+    url: "https://webpack.js.org/",
+  },
+  {
+    name: "Vuetify",
+    url: "https://vuetifyjs.com/en/",
+  },
+  {
+    name: "AntDesign",
+    url: "https://ant.design/components/drawer-cn/",
+  },
+  {
+    name: "NaiveUI",
+    url: "https://www.naiveui.com/zh-CN/os-theme/components/button",
+  },
+];
